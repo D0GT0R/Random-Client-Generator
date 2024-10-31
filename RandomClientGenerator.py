ee7fdcb65afc9656fb20e4261f6edb4b9edb30bd
Caleb <poole.caleb@gmail.com>
2024-10-31 12:09:08 -0400

    Update RandomClientGenerator.py

--- RandomClientGenerator.py
+++ RandomClientGenerator.py
@@ -11,28 +11,32 @@ sheet = workbook.sheet_by_name('Sheet1')
 inp = input('How many clients are needed?\n')
 clientCount = int(inp)
 
+inp2 = input('User requesting clients:\n')
+
 today = date.today()
 
-todayDate = today.strftime("%b-%d-%y")
+todaysDate = today.strftime("%b-%d-%y")
 
-workbook = xlsxwriter.Workbook('Clients ' + todayDate + '.xlsx')
-worksheet = workbook.add_worksheet()
+workbook1 = xlsxwriter.Workbook(inp2 + ' Clients ' + todaysDate + '.xlsx')
+worksheet = workbook1.add_worksheet()
 
 row = 0
 col = 0
+
+worksheet.set_column(0, 5, 25)
 
 worksheet.write(row, col, 'First Name')
 worksheet.write(row, col + 1, 'Last Name')
 worksheet.write(row, col + 2, 'Address 1')
 worksheet.write(row, col + 3, 'City')
-worksheet.write(row, col + 4, 'State')
-worksheet.write(row, col + 5, 'Zipcode')
+worksheet.write(row, col + 4, 'Zip Code')
+worksheet.write(row, col + 5, 'State')
 
 row = 1
 
 for x in range(clientCount):
 
-    randRow = random.randint(1, 820)
+    randRow = random.randint(1, 2480)
     selectRow = int(randRow)
 
     iterationValue = (x + 1)
@@ -41,6 +45,7 @@ for x in range(clientCount):
     city = sheet.cell(selectRow, 1).value
     zipCode = sheet.cell(selectRow, 2).value
     state = sheet.cell(selectRow, 3).value
+    
 
     clientFirst = names.get_first_name()
     clientLast = names.get_last_name()
@@ -56,4 +61,4 @@ for x in range(clientCount):
 
 print('The list has been genrated with ' + inp + ' clients.')
 
-workbook.close()
+workbook1.close()
