f1e8d423463e13a265d5d8d32ae0baad19b1d7e1
Caleb <poole.caleb@gmail.com>
2024-11-05 08:22:32 -0500

    Update RandomClientGenerator.py
    Swapping from 'random' library to 'secrets'

--- RandomClientGenerator.py
+++ RandomClientGenerator.py
@@ -1,20 +1,17 @@
 from datetime import date
 import names
-import random
 import re
+import secrets
 import xlrd
 import xlsxwriter
 
-###---Sanitize Input Logic---###
 def sanitize_input(input_str):
     sanitized_str = re.sub(r'<script\b[^>]*>(.*?)</script>', '', input_str, flags=re.IGNORECASE)
     return sanitized_str
 
-###---Read spreadsheet for address data---###
 workbook1 = xlrd.open_workbook('AddressData.xls')
 sheet = workbook1.sheet_by_name('Sheet1')
 
-###---Check for/allow numeric values only for client quantity---###
 while True:
     try:
         clientCountInt = int(sanitize_input(input('How many clients are needed?\n')))
@@ -23,16 +20,13 @@ while True:
         continue
     break
 
-###---Enter Username (For file name)---###
 userNameInput = sanitize_input(input('User requesting clients:\n'))
 
 todaysDate = date.today().strftime("%b-%d-%y")
 
-###---Create Excel File---###
 workbook2 = xlsxwriter.Workbook(userNameInput + ' Clients ' + todaysDate + '.xlsx')
 worksheet = workbook2.add_worksheet()
 
-###---Function to set headers (needs work)---###
 def set_headers():
 
     row = 0
@@ -47,16 +41,17 @@ def set_headers():
     worksheet.write(row, col + 4, 'Zip Code')
     worksheet.write(row, col + 5, 'State')
 
-###---Call Headers Function---###
 set_headers()
+
+randInt = secrets.SystemRandom()
 
 row = 1
 col = 0
 
-###---For Loop to populate the Excel file with clients---###
 for x in range(clientCountInt):
 
-    randRow = random.randint(1, 10000)
+    randRow = randInt.randrange(1, 10000)
+    
     selectRow = int(randRow)
 
     add1 = sheet.cell(selectRow, 0).value
@@ -76,7 +71,6 @@ for x in range(clientCountInt):
 
     row += 1
 
-###---Close/Save the created Excel file---###
 workbook2.close()
 
 print('The list has been genrated with ' + str(clientCountInt) + ' clients.')
